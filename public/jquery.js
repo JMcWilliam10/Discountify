28b05d12ef8e93b903231c6405c98f0de0748d46
Jonathan McWilliam <jonathanmcwilliam@gmail.com>
2018-12-10 17:41:44 -0500

    testing mobile phone template

---
+++ public/jquery.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#flip").click(function(){
+        $("#panel").slideToggle();
+    });
+});
+
+$(document).ready(function(){
+    $(".home").hide(0).delay(500).fadeIn(3000)
+});
